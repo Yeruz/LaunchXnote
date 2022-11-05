39b0924e7b094d031c5863d7153ee4a682847109
yeruz <lipitho13@gmail.com>
2022-11-05 14:42:51 -0600

    modified files

--- pokedex.js
+++ pokedex.js
@@ -1,28 +1,33 @@
-const fetchPokemon = () => {
+const fetchPokemon = async () => {
   const pokeNameInput = document.getElementById("pokeName");
   let pokeName = pokeNameInput.value;
   pokeName = pokeName.toLowerCase();
   const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
-  fetch(url)
-    .then((res) => {
-      if (res.status != "200") {
-        console.log(res);
-        pokeImage("./img/tenor.gif");
-      } else {
-        return res.json();
-      }
-    })
-    .then((data) => {
-      if (data) {
-        console.log(data);
-        let pokeImg = data.sprites.front_default;
-        pokeImage(pokeImg);
-        console.log(pokeImg);
-      }
-    });
+  let data = await fetch(url).then((res) => {
+    if (res.status != "200") {
+      console.log(res);
+      pokeImage("./img/tenor.gif");
+    } else {
+      return res.json();
+    }
+  });
+  if (data) {
+    console.log(data);
+    let pokeImg = data.sprites.front_default;
+    let pokeInfo = data.abilities;
+    pokeImage(pokeImg);
+    pokeData(pokeInfo);
+    console.log(pokeImg);
+  }
 };
 
 const pokeImage = (url) => {
   const pokePhoto = document.getElementById("pokeImg");
   pokePhoto.src = url;
+};
+
+const pokeData = (abilities) => {
+  const pokeInfo = document.getElementById("habilities");
+  const abilitiesName = abilities.map((item) => item.abilities.name);
+  pokeInfo.innerHTML = abilitiesName;
 };
